5fa77ac56fb4e6e0d26fa3f9b345f24cd1ad1989
Aavishkar-Kolte <aavishkarkolte@gmail.com>
2024-09-22 22:06:23 +0530

    fix: add changes to save replay file in a organized mannar
    - Updated the Grid1x1DemoEnv class to manage the saving of replay files in a more organized manner.
    - Saving replay files based on the environment name, timestamp and episode.
    - Updated few paths in config.json of Grid1x1DemoEnv

--- gym_cityflow/envs/grid_1x1/grid_1x1_demo_env/env.py
+++ gym_cityflow/envs/grid_1x1/grid_1x1_demo_env/env.py
@@ -4,16 +4,30 @@ from gymnasium.spaces import Box
 import cityflow
 import numpy as np
 import json
+from datetime import datetime
 
 
 class Grid1x1DemoEnv(gym.Env):
     def __init__(self, thread_num=1, time_steps=3600):
         # Set up the environment configuration
+        self.env_name = "Grid1x1DemoEnv"
+        os.makedirs("replay_files", exist_ok=True)
+        os.makedirs(os.path.join(os.getcwd(), "replay_files", self.env_name), exist_ok=True)
+
+        # Get the current timestamp
+        timestamp = datetime.now()
+        timestamp_string = timestamp.strftime("%Y%m%d_%H%M%S")
+        os.makedirs(os.path.join(os.getcwd(), "replay_files", self.env_name, timestamp_string), exist_ok=True)
+
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         config_file_path = os.path.join(self.current_dir, 'config_files', 'config.json')
         self.engine = cityflow.Engine(config_file=config_file_path, thread_num=thread_num)
+
         self.time_steps = time_steps
         self.current_episode = 0
+
+        self.replay_files_dir_path = os.path.join(os.getcwd(), "replay_files", self.env_name, timestamp_string)
+        self.engine.set_replay_file(os.path.join(self.replay_files_dir_path, f"replay_{self.current_episode}.txt"))
 
         # Load the road network data
         roadnet_file_path = os.path.join(self.current_dir, "config_files", "roadnet.json")
@@ -49,7 +63,7 @@ class Grid1x1DemoEnv(gym.Env):
             high=int64_max_value,
             shape=(self.observation_space_length,),
             dtype=np.int64
-        )
+        )        
 
 
     def step(self, action):
