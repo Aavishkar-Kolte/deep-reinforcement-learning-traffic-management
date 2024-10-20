12d3ca5a5281f47b614bdec044f70481048232f9
Aavishkar-Kolte <aavishkarkolte@gmail.com>
2024-10-20 14:04:17 +0530

    Changed observation space from multidiscrete to box space
    - After training models for both box and multidiscrete, i found that
      training box space is very faster as compared to multidiscrete

--- train_ppo.py
+++ train_ppo.py
@@ -6,16 +6,16 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 from gym_cityflow.envs.grid_1x1.grid_1x1_demo_env import Grid1x1DemoEnv
 
 
-env = Grid1x1DemoEnv(max_timesteps=1000)
+env = Grid1x1DemoEnv(max_timesteps=600, save_replay=False)
 check_env(env)
 
 env = Monitor(env)
 env = DummyVecEnv([lambda: env])
 
 # Define a checkpoint callback to save models during training
-checkpoint_callback = CheckpointCallback(save_freq=100000, save_path='./models/', name_prefix='ppo_traffic_agent')
+checkpoint_callback = CheckpointCallback(save_freq=300000, save_path='./models/', name_prefix='ppo_traffic_agent')
 
-model = PPO("MultiInputPolicy", env, verbose=1, tensorboard_log="./ppo_tensorboard/")
+model = PPO("MlpPolicy", env, ent_coef=0.01 ,verbose=1, tensorboard_log="./ppo_tensorboard/")
 
-model.learn(total_timesteps=5000000, callback=checkpoint_callback)
-model.save("ppo_traffic")
+model.learn(total_timesteps=1200000, callback=checkpoint_callback)
+model.save("ppo_traffic3")
